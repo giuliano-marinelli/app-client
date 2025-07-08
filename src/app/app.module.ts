4781e03ff8fc13d158adc23980610edc2295b33e
Giuliano Marinelli <giuliano.marinelli@hotmail.com>
2025-07-08 13:28:38 -0300

    updating angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@
 //angular modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { HttpHeaders, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -68,208 +68,190 @@ import { DevicesSettingsComponent } from './account/devices-settings/devices-set
 import { AdminComponent } from './admin/admin.component';
 import { UsersAdminComponent } from './admin/users-admin/users-admin.component';
 
-@NgModule({
-  declarations: [
-    //directives
-    VarDirective,
-    //pipes
-    FilterPipe,
-    SortPipe,
-    TruncatePipe,
-    //shared components
-    ConfirmComponent,
-    LeaveGuardWarningComponent,
-    SearchComponent,
-    InvalidFeedbackComponent,
-    VerifiedMarkComponent,
-    //components
-    AppComponent,
-    NotFoundComponent,
-    LoginComponent,
-    RegisterComponent,
-    PasswordResetComponent,
-    ProfileComponent,
-    AccountComponent,
-    ProfileSettingsComponent,
-    AccountSettingsComponent,
-    EmailsSettingsComponent,
-    SecuritySettingsComponent,
-    DevicesSettingsComponent,
-    AdminComponent,
-    UsersAdminComponent
-  ],
-  imports: [
-    //angular modules
-    CommonModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    HammerModule,
-    //graphql modules
-    ApolloModule,
-    //modules
-    AppRoutingModule,
-    NgbModule,
-    FontAwesomeModule,
-    ImageCropperModule,
-    NarikCustomValidatorsModule,
-    NgOtpInputModule,
-    NgxMasonryModule,
-    AngularResizeEventModule,
-    JwtModule.forRoot({
-      config: {
-        tokenGetter: (): string | null => localStorage.getItem('token')
-        // whitelistedDomains: ['localhost:3000', 'localhost:4200']
-      }
-    }),
-    InputMaskModule.forRoot({
-      inputSelector: 'input',
-      isAsync: true
-    }),
-    MomentModule.forRoot({
-      relativeTimeThresholdOptions: {
-        m: 59
-      }
-    }),
-    //directives
-    TippyDirective
-  ],
-  providers: [
-    //configurations
-    provideTippyConfig({
-      defaultVariation: 'tooltip',
-      variations: {
-        tooltip: {
-          theme: 'material',
-          arrow: true,
-          maxWidth: 200,
-          animation: 'scale',
-          trigger: 'mouseenter',
-          offset: [0, 5]
-        },
-        popper: {
-          theme: 'material',
-          arrow: true,
-          maxWidth: 200,
-          animation: 'scale',
-          trigger: 'click',
-          offset: [0, 5]
-        }
-      }
-    }),
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory(httpLink: HttpLink, messages: MessagesService) {
-        //create an http link
-        const http = httpLink.create({
-          uri: environment.graphqlHTTPUri,
-          //required for upload files
-          extractFiles: (body) => {
-            return extractFiles(body, isExtractableFile) as any;
-          },
-          //here we can set the headers for all http requests
-          headers: new HttpHeaders().set('apollo-require-preflight', 'true') //required for upload files
-        });
-
-        //create a websocket link (if it's needed for subscriptions)
-        const ws = new GraphQLWsLink(
-          createClient({
-            url: environment.graphqlWSUri
-          })
-        );
-
-        //add authentication token to the headers
-        const auth = setContext(() => {
-          //get the authentication token from local storage if it exists
-          const token = localStorage.getItem('token');
-
-          //add the token to the headers
-          return token
-            ? {
-                headers: {
-                  Authorization: `Bearer ${token}`
-                }
-              }
-            : {};
-        });
-
-        //general error handling behavior (for all requests)
-        //specifically for network errors that only shows from here
-        const error = onError(({ graphQLErrors, networkError }) => {
-          if (graphQLErrors) {
-            graphQLErrors.map((error: any) => console.error('[GraphQL error]', error));
-          }
-
-          if (networkError) {
-            console.error('[Network error]', networkError);
-            messages.error('<b>Network error</b>. Please check your internet connection.', {
-              icon: 'fas fa-wifi',
-              container: 'network-error',
-              position: 'bottomCenter',
-              onlyOne: true,
-              displayMode: 'replace'
-            });
-          }
-        });
-
-        //general success handling behavior (for all requests)
-        const success = new ApolloLink((operation, forward) => {
-          return forward(operation).map((data) => {
-            //clear the network error messages
-            messages.clear('network-error');
-            return data;
-          });
-        });
-
-        //split for set the link type (http or ws) based on the operation type
-        //add the auth context to the link
-        //set the general error handling behavior
-        const link = ApolloLink.from([
-          auth,
-          error,
-          success,
-          split(
-            //split based on operation type
-            ({ query }) => {
-              const definition = getMainDefinition(query);
-              return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
-            },
-            ws,
-            http
-          )
-        ]);
-
-        return {
-          link: link,
-          cache: new InMemoryCache({
-            addTypename: false
-          }),
-          defaultOptions: {
-            watchQuery: {
-              fetchPolicy: 'cache-and-network',
-              errorPolicy: 'all'
-            },
-            query: {
-              fetchPolicy: 'network-only',
-              errorPolicy: 'all'
-            },
-            mutate: {
-              fetchPolicy: 'network-only',
-              errorPolicy: 'all'
+@NgModule({ declarations: [
+        //directives
+        VarDirective,
+        //pipes
+        FilterPipe,
+        SortPipe,
+        TruncatePipe,
+        //shared components
+        ConfirmComponent,
+        LeaveGuardWarningComponent,
+        SearchComponent,
+        InvalidFeedbackComponent,
+        VerifiedMarkComponent,
+        //components
+        AppComponent,
+        NotFoundComponent,
+        LoginComponent,
+        RegisterComponent,
+        PasswordResetComponent,
+        ProfileComponent,
+        AccountComponent,
+        ProfileSettingsComponent,
+        AccountSettingsComponent,
+        EmailsSettingsComponent,
+        SecuritySettingsComponent,
+        DevicesSettingsComponent,
+        AdminComponent,
+        UsersAdminComponent
+    ],
+    bootstrap: [AppComponent], imports: [
+        //angular modules
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HammerModule,
+        //graphql modules
+        ApolloModule,
+        //modules
+        AppRoutingModule,
+        NgbModule,
+        FontAwesomeModule,
+        ImageCropperModule,
+        NarikCustomValidatorsModule,
+        NgOtpInputModule,
+        NgxMasonryModule,
+        AngularResizeEventModule,
+        JwtModule.forRoot({
+            config: {
+                tokenGetter: (): string | null => localStorage.getItem('token')
+                // whitelistedDomains: ['localhost:3000', 'localhost:4200']
             }
-          }
-        };
-      },
-      deps: [HttpLink, MessagesService]
-    },
-    //services
-    MessagesService,
-    AuthService
-  ],
-  bootstrap: [AppComponent]
-})
+        }),
+        InputMaskModule.forRoot({
+            inputSelector: 'input',
+            isAsync: true
+        }),
+        MomentModule.forRoot({
+            relativeTimeThresholdOptions: {
+                m: 59
+            }
+        }),
+        //directives
+        TippyDirective], providers: [
+        //configurations
+        provideTippyConfig({
+            defaultVariation: 'tooltip',
+            variations: {
+                tooltip: {
+                    theme: 'material',
+                    arrow: true,
+                    maxWidth: 200,
+                    animation: 'scale',
+                    trigger: 'mouseenter',
+                    offset: [0, 5]
+                },
+                popper: {
+                    theme: 'material',
+                    arrow: true,
+                    maxWidth: 200,
+                    animation: 'scale',
+                    trigger: 'click',
+                    offset: [0, 5]
+                }
+            }
+        }),
+        {
+            provide: APOLLO_OPTIONS,
+            useFactory(httpLink: HttpLink, messages: MessagesService) {
+                //create an http link
+                const http = httpLink.create({
+                    uri: environment.graphqlHTTPUri,
+                    //required for upload files
+                    extractFiles: (body) => {
+                        return extractFiles(body, isExtractableFile) as any;
+                    },
+                    //here we can set the headers for all http requests
+                    headers: new HttpHeaders().set('apollo-require-preflight', 'true') //required for upload files
+                });
+                //create a websocket link (if it's needed for subscriptions)
+                const ws = new GraphQLWsLink(createClient({
+                    url: environment.graphqlWSUri
+                }));
+                //add authentication token to the headers
+                const auth = setContext(() => {
+                    //get the authentication token from local storage if it exists
+                    const token = localStorage.getItem('token');
+                    //add the token to the headers
+                    return token
+                        ? {
+                            headers: {
+                                Authorization: `Bearer ${token}`
+                            }
+                        }
+                        : {};
+                });
+                //general error handling behavior (for all requests)
+                //specifically for network errors that only shows from here
+                const error = onError(({ graphQLErrors, networkError }) => {
+                    if (graphQLErrors) {
+                        graphQLErrors.map((error: any) => console.error('[GraphQL error]', error));
+                    }
+                    if (networkError) {
+                        console.error('[Network error]', networkError);
+                        messages.error('<b>Network error</b>. Please check your internet connection.', {
+                            icon: 'fas fa-wifi',
+                            container: 'network-error',
+                            position: 'bottomCenter',
+                            onlyOne: true,
+                            displayMode: 'replace'
+                        });
+                    }
+                });
+                //general success handling behavior (for all requests)
+                const success = new ApolloLink((operation, forward) => {
+                    return forward(operation).map((data) => {
+                        //clear the network error messages
+                        messages.clear('network-error');
+                        return data;
+                    });
+                });
+                //split for set the link type (http or ws) based on the operation type
+                //add the auth context to the link
+                //set the general error handling behavior
+                const link = ApolloLink.from([
+                    auth,
+                    error,
+                    success,
+                    split(
+                    //split based on operation type
+                    ({ query }) => {
+                        const definition = getMainDefinition(query);
+                        return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+                    }, ws, http)
+                ]);
+                return {
+                    link: link,
+                    cache: new InMemoryCache({
+                        addTypename: false
+                    }),
+                    defaultOptions: {
+                        watchQuery: {
+                            fetchPolicy: 'cache-and-network',
+                            errorPolicy: 'all'
+                        },
+                        query: {
+                            fetchPolicy: 'network-only',
+                            errorPolicy: 'all'
+                        },
+                        mutate: {
+                            fetchPolicy: 'network-only',
+                            errorPolicy: 'all'
+                        }
+                    }
+                };
+            },
+            deps: [HttpLink, MessagesService]
+        },
+        //services
+        MessagesService,
+        AuthService,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {
   constructor(fontawesomeLibrary: FaIconLibrary, fontawesomeConfig: FaConfig) {
     fontawesomeLibrary.addIconPacks(far, fas, fab);
